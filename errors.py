a932075cd65572a719cba383cec1c3a35f80b926
gmguarino <gmguarino1@gmail.com>
2023-12-01 14:18:38 +0100

    new error

--- errors.py
+++ errors.py
@@ -14,6 +14,15 @@ class IdNotSpecifiedError(Exception):
     """Exception raised for not specifying either page id or the database id.
     """
 
-    def __init__(self, key):
+    def __init__(self):
         message = "Please provide either a database ID or a page id where the database can be created."
         super().__init__(message)
+
+
+class TemplateNotSpecifiedError(Exception):
+    """Exception raised for not specifying a template for the database to be created
+    """
+
+    def __init__(self):
+        message = "Please provide a template for the database or a template filepath."
+        super().__init__(message)       
