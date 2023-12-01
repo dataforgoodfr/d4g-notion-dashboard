ab3b92b61e7af10b89a992e5c8f4ce53609250fe
gmguarino <gmguarino1@gmail.com>
2023-12-01 14:19:29 +0100

    add db serach and get or create

--- database_handler.py
+++ database_handler.py
@@ -1,5 +1,6 @@
-from errors import KeyNotFoundError, IdNotSpecifiedError
+from errors import KeyNotFoundError, IdNotSpecifiedError, TemplateNotSpecifiedError
 
+from time import sleep
 import requests
 import json
 import os
@@ -9,13 +10,13 @@ class NotionDatabaseHandler(object):
     """
     General class to handle the connection to a Notion Database.
     """
-    def __init__(self, notion_token, database_id=None, page_id=None, template_dir="templates") -> None:
-        if database_id is None and page_id is None:
+    def __init__(self, notion_token, database_id=None, parent_page_id=None, template_dir="templates") -> None:
+        if database_id is None and parent_page_id is None:
             raise IdNotSpecifiedError
 
         self.notion_token = notion_token
         self.database_id = database_id
-        self.page_id = page_id
+        self.parent_page_id = parent_page_id
         self.template_dir = template_dir
 
         self.headers = {
@@ -24,22 +25,29 @@ class NotionDatabaseHandler(object):
             "Notion-Version": "2022-06-28",
         }
 
-    def create_database(self, template_file):
+    def create_database(self, template_file=None, json_template=None):
         """
         Creates the database from a json template file and the parent page id
         """
-        assert self.page_id is not None
-        with open(os.path.join(self.template_dir, template_file)) as jf:
-            config = json.load(jf)
+        assert self.parent_page_id is not None
+
+        if json_template is not None:
+            config = json_template
+        elif template_file is not None:
+            with open(os.path.join(self.template_dir, template_file)) as jf:
+                config = json.load(jf)
+        else:
+            raise TemplateNotSpecifiedError
+
         config.update({
             "parent": {
                 "type": "page_id",
-                "page_id": self.page_id
+                "page_id": self.parent_page_id
             }
         })
         url = "https://api.notion.com/v1/databases"
         response = requests.post(url, json=config, headers=self.headers)
-        return response.json()
+        return {"database_id": response.json()['id']}
     
     def create_entry(self, database_id, entry: dict):
         """
@@ -58,6 +66,7 @@ class NotionDatabaseHandler(object):
         """
         results = {}
         for entry in records:
+            
             res = self.create_entry(database_id, entry)
             print(res.status_code)
             results.update(res.json())
@@ -123,8 +132,7 @@ class NotionDatabaseHandler(object):
         res = requests.patch(url, json=payload, headers=self.headers)
         return res
     
-
-    def update_database(self, config: dict):
+    def update_database_entries(self, config: dict):
         """
         Updates the entries in the database using key/value pairs
         """
@@ -134,3 +142,86 @@ class NotionDatabaseHandler(object):
             page = self.update_numerical_value(page, value)
             res = self.update_page(page)
             print(res.status_code)
+
+    def update_database_complete(self, config: dict):
+        pages = self.get_pages()
+        # TODO
+        pass
+
+    def search_database_by_title(self, title:str):
+        """
+        Searches for a database with the specified title associated with the 
+        parent page id
+        """
+        payload = {
+            "query":f"{title}",
+            "filter": {
+                "value": "database",
+                "property": "object"
+            },
+            "sort":{
+                "direction":"ascending",
+                "timestamp":"last_edited_time"
+            }
+        }
+
+        search_url = "https://api.notion.com/v1/search"
+
+        response = requests.post(search_url, headers=self.headers, json=payload)
+        data = response.json()
+        results = data["results"]
+        while data["has_more"]:
+            payload = {"page_size": 100, "start_cursor": data["next_cursor"]}
+            response = requests.post(search_url, json=payload, headers=self.headers)
+            data = response.json()
+            results.extend(data["results"])
+
+        
+        for result in results:
+            if result["parent"]["type"] == "page_id":
+                if result["parent"]["page_id"] == os.environ.get("PAGE_ID"):
+                    print(result['id'], result["title"][0]["text"]["content"])
+                    return {"database_id": result["id"]}
+
+        return {"database_id": None}
+    
+    def get_or_create_database(self, template_file, records):
+        with open(os.path.join(self.template_dir, template_file)) as jf:
+            config = json.load(jf)
+        title = config["title"][0]["text"]["content"]
+        database_id = self.search_database_by_title(title)['database_id']
+        if database_id is None:
+            database_id = self.create_database(json_template=config)["database_id"]
+        return database_id
+
+
+
+if __name__=="__main__":
+    # Testing the search API
+    from dotenv import load_dotenv
+
+    load_dotenv("creds.env")
+
+    NOTION_TOKEN = os.environ.get("NOTION_TOKEN")
+    headers = {
+        "Authorization": "Bearer " + NOTION_TOKEN,
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-06-28",
+    }
+    data = {
+        "query":"Repository Activity",
+        "filter": {
+            "value": "database",
+            "property": "object"
+        },
+        "sort":{
+            "direction":"ascending",
+            "timestamp":"last_edited_time"
+        }
+    }
+
+    response = requests.post("https://api.notion.com/v1/search", headers=headers, json=data)
+    for result in response.json()['results']:
+        if result["parent"]["type"] == "page_id":
+            if result["parent"]["page_id"] == os.environ.get("PAGE_ID"):
+                print(result['id'], result["title"][0]["text"]["content"])
