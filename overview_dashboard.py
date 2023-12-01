a166222b7cb2bd0c90fcc62cf97fb7e18b070d87
gmguarino <gmguarino1@gmail.com>
2023-12-01 16:01:53 +0100

    finish overview dashboard

--- overview_dashboard.py
+++ overview_dashboard.py
@@ -2,21 +2,13 @@ from database_handler import NotionDatabaseHandler
 
 class GithubOverviewDashboard(NotionDatabaseHandler):
 
-    def __init__(self, notion_token, database_id=None, page_id=None, template_dir="templates", template_file="overview.json") -> None:
+    def __init__(self, notion_token, parent_page_id=None, database_id=None, template_dir="templates", template_file="overview.json") -> None:
         self.template_file = template_file
-        super().__init__(notion_token, database_id, page_id, template_dir)
+        super().__init__(notion_token, parent_page_id, database_id, template_dir)
     
-    def create_dashboard(self):
-        if self.database_id is None:
-            self.database_id = self.create_database(self.template_file)['id']
-            self.populate_database(
-                database_id=self.database_id, 
-                records=[
-                    {"Event": {"title": [{"type":"text", "text": {"content": "Contributors"}}]}, "Commits": {"number": 0}},
-                    {"Event": {"title": [{"type":"text", "text": {"content": "PRs"}}]}, "Commits": {"number": 0}},
-                    {"Event": {"title": [{"type":"text", "text": {"content": "Commits"}}]}, "Commits": {"number": 0}},
-                ]
-            )
+    def create_dashboard(self, records):
+        self.database_id = self.get_or_create_database(self.template_file, records)
+        return self.database_id
 
     def update_dashboard(self, commits=0, pr=0, contributors=0):
         self.update_database_entries({
